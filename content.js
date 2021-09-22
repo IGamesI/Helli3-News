aefbc96c175c7fd02da78face9604c387fd254ae
IGamesI <69199901+IGamesI@users.noreply.github.com>
2021-09-22 09:37:05 +0330

    Added a task complete button
    KNOWN ISSUE: the button press doesn't work.

---
+++ content.js
@@ -0,0 +1,9 @@
+console.log("Test kinda sucess!");
+// Todo: render each item from items list and do the function below for every one
+// Get the complete button
+const compButtton = document.querySelector('.task-com-btn');
+
+compButtton.addEventListener('click', () => {
+    console.log("Test sucess!")
+});
+

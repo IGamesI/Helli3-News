fdf7aca94bdfb57d3ab00f7b661e32860d678934
IGamesI <69199901+IGamesI@users.noreply.github.com>
2021-09-22 16:48:51 +0330

    Revamped UI
    Changed include:
    - A new, news componnent with revamped UI
    - A news header with a light gray background
    - Scrollable news section
    - The title color of each news can be changed

--- content.js
+++ content.js
@@ -1,9 +1,1 @@
 console.log("Test kinda sucess!");
-// Todo: render each item from items list and do the function below for every one
-// Get the complete button
-const compButtton = document.querySelector('.task-com-btn');
-
-compButtton.addEventListener('click', () => {
-    console.log("Test sucess!")
-});
-
